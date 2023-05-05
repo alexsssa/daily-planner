6dd66a5b7e493d4b7df95a6eec9a9aaa220acfa7
Alicia <Alicia@WIN-JO22SGESGIG>
2023-05-05 22:01:53 +0400

    Добавьте файлы проекта.

---
+++ WpfApp3/Autorization.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp3
+{
+    public partial class Autorization : Window
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void button_done(object sender, RoutedEventArgs e)
+        {
+            /*if (inputlogin.Text.Length > 0)
+            {
+                if (inputpassword.Password.Length > 0)
+                {
+                    //поиск в БД
+                    new MenuWindow().Show();
+                    this.Close();
+                }
+                else { MessageBox.Show("Ошибка: Проверьте поле ввода пароля", "Возникла ошибка!"); }
+            }
+            else { MessageBox.Show("Ошибка: Проверьте поле ввода логина", "Возникла ошибка!"); }*/
+        }
+    }
+}
